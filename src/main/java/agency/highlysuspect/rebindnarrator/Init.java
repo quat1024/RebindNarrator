c9cc00fe36afd4d93a3e6786452a5231ec6c21ec
quat1024 <quat1024@users.noreply.github.com>
2021-06-08 23:01:54 -0400

    port to 1.17.0

--- src/main/java/agency/highlysuspect/rebindnarrator/Init.java
+++ src/main/java/agency/highlysuspect/rebindnarrator/Init.java
@@ -2,7 +2,7 @@ package agency.highlysuspect.rebindnarrator;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
